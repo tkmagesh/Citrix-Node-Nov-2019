5b2b9a343decc1e754a7f583927138926e9b4e57
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-25 14:33:35 +0530

    accumulator - solution

---
+++ 03-modularity/accumulator.js
@@ -0,0 +1,23 @@
+
+module.exports = function(defaultResult = 0){
+
+    var result = defaultResult;
+
+    return {
+        add(x){
+            result += x;
+        },
+        subtract(x){
+            result -= x;
+        },
+        multiply(x){
+            result *= x;
+        },
+        divide(x){
+            result /= x;
+        },
+        getResult(){
+            return result;
+        }
+    };
+};
