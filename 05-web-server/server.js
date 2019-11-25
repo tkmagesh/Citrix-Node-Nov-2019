34ff761bbeabc268df58433f0710ba284d8a90b0
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-25 16:01:00 +0530

    web server - initial

---
+++ 05-web-server/server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+// req : IncomingMessage -> ReadableStream
+// res : ServerResponse -> WritableStream
+var server = http.createServer(function(req, res){
+    res.write('<h1>Welcome to Node.js</h1>');
+    res.end();
+});
+
+server.listen(8080);
+
+server.on('listening', function(){
+    console.log('server listening on port 8080!!');
+});
