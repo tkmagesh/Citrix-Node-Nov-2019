915b5f0083324f33ed0333d2f0bc8da8893f3a01
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-25 16:44:13 +0530

    Day-01 complete

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,10 +1,22 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
 
 // req : IncomingMessage -> ReadableStream
 // res : ServerResponse -> WritableStream
 var server = http.createServer(function(req, res){
-    res.write('<h1>Welcome to Node.js</h1>');
-    res.end();
+    var urlObj = url.parse(req.url),
+        resourceName = urlObj.pathname,
+        resourceFullName = path.join(__dirname, resourceName);
+    console.log(resourceName);
+    if (!fs.existsSync(resourceFullName)){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    var stream = fs.createReadStream(resourceFullName);
+    stream.pipe(res);
 });
 
 server.listen(8080);
