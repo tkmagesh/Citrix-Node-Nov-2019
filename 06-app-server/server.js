5670c6524f92c2ba675ccbef8423811afafaeb12
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-26 09:54:31 +0530

    app server (for calculator)

---
+++ 06-app-server/server.js
@@ -0,0 +1,25 @@
+var http = require('http'),
+    url = require('url'),
+    querystring = require('querystring'),
+    calculator = require('./calculator');
+
+var server = http.createServer(function(req, res){
+    var urlObj = url.parse(req.url);
+    if (urlObj.pathname !== '/calculator'){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    var queryData = querystring.parse(urlObj.query),
+        x = parseInt(queryData.x),
+        y = parseInt(queryData.y),
+        op = queryData.op;
+    var result = calculator[op](x,y);
+    res.write(result.toString());
+    res.end();
+});
+
+server.listen(8080);
+server.on('listening', function(){
+    console.log('app server listening on 8080');
+});
