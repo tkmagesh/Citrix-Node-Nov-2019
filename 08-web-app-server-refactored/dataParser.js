53899b638eade03ebd7318bb4a9e57c2f5ec9ef2
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-26 14:19:13 +0530

    refactoring data parsing responsibility (for POST requests) to dataParser

--- 08-web-app-server-refactored/dataParser.js
+++ 08-web-app-server-refactored/dataParser.js
@@ -3,6 +3,13 @@ var url = require('url'),
 
 module.exports = function(req, res, next){
     req['urlObj'] = url.parse(req.url);
-    req['queryData'] = querystring.parse(req.urlObj.query),
-    next();
+    req['queryData'] = querystring.parse(req.urlObj.query);
+    var rawData = '';
+    req.on('data', function (chunk) {
+        rawData += chunk;
+    });
+    req.on('end', function () {
+        req['bodyData'] = querystring.parse(rawData);
+        next();
+    });
 };
--- 08-web-app-server-refactored/serveCalculator.js
+++ 08-web-app-server-refactored/serveCalculator.js
@@ -1,33 +1,15 @@
-var querystring = require('querystring'),
-    calculator = require('./calculator');
+var calculator = require('./calculator');
     
 module.exports = function(req, res, next){
     if (req.urlObj.pathname === '/calculator') {
-        if (req.method === 'GET') {
-            var queryData = req.queryData,
-                x = parseInt(queryData.x),
-                y = parseInt(queryData.y),
-                op = queryData.op;
-            var result = calculator[op](x, y);
-            res.write(result.toString());
-            res.end();
-            next();
-        } else {
-            var rawData = '';
-            req.on('data', function (chunk) {
-                rawData += chunk;
-            });
-            req.on('end', function () {
-                var bodyData = querystring.parse(rawData),
-                    x = parseInt(bodyData.x),
-                    y = parseInt(bodyData.y),
-                    op = bodyData.op;
-                var result = calculator[op](x, y);
-                res.write(result.toString());
-                res.end();
-                next();
-            });
-        } 
+        var data = req.method === 'GET' ? req.queryData : req.bodyData;
+        var x = parseInt(data.x),
+            y = parseInt(data.y),
+            op = data.op;
+        var result = calculator[op](x, y);
+        res.write(result.toString());
+        res.end();
+        next();
     } else {
         next();
     }
