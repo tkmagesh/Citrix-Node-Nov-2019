13e56893e8e457579ce7c0dcb4cf68ecaf7761b1
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-26 16:25:50 +0530

    Day-02 complete

--- 08-web-app-server-refactored/logger.js
+++ 08-web-app-server-refactored/logger.js
@@ -1,4 +1,13 @@
+var chalk = require('chalk');
+
 module.exports = function(req, res, next){
-    console.log(req.method + '\t' + req.urlObj.pathname);
+    var logMessage = chalk.cyanBright(req.method) + '\t' + req.urlObj.pathname;
+    var start = new Date();
+    res.on('finish', function(){
+        var end = new Date(),
+            elapsed = end - start;
+        logMessage += '\t' + chalk.red(res.statusCode) + '\t' + chalk.greenBright(elapsed) + 'ms';
+        console.log(logMessage);
+    });
     next();
 }
